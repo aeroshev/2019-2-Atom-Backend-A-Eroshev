96fac47d7f56ebc3ff3ec44555ab93508a7ffe71
Артём <artem@Artems-MacBook-Pro.local>
2019-12-02 14:00:37 +0300

    Cleanup workspace

--- messenger/templates/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-import './components/FormInput';
-import './components/DialogInformation';
-import './components/MessageBox';
-import './components/MessageForm';
-import './components/DialogList';
-import './components/DialogBox';
-import './components/GetDIalog';
