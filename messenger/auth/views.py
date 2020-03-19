0b08333134c4d53aefd4581a7af3f4b15b722ad5
Ubuntu <aeroshev@mail.ru>
2020-03-19 10:00:30 +0000

    Add redirect to SPA frontend

--- messenger/auth/views.py
+++ messenger/auth/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def home(request):
-    return render(request, 'registration/home.html')
+    # return render(request, 'registration/home.html')
+    return redirect('https://aeroshev.github.io/2019-2-Atom-Frontend-A-Eroshev/')
